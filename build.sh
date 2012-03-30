3cc8cf74ed57b3af476b06ae41de175da9aafb0c
Seppi <josefnpat@hotmail.com>
2012-03-30 18:33:38 -0400

    Setting a baseline version of wkhtmltopdf (0.11.0)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Use US Letter head.
 cd src
 php index.php > temp.html
-wkhtmltopdf -s Letter -d 320 -B 0 -L 0 -R 0 -T 0 temp.html ../resume.pdf
+../wkhtmltopdf -s Letter -d 320 -B 0 -L 0 -R 0 -T 0 temp.html ../resume.pdf
 rm temp.html
 cd ..
 
--- dep.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# This file represents the depedencies for building this resume.
-apt-get install wkhtmltopdf php5
