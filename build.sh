45a719378d964215764a835bd878b65ca65b0d36
Seppi <josefnpat@hotmail.com>
2012-06-12 21:53:47 -0400

    Adding the git tag to resume output

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# Use US Letter head.
+GIT=`git log --pretty=format:'%h' -n 1`
 cd src
 php index.php > temp.html
-../wkhtmltopdf -s Letter -d 320 -B 0 -L 0 -R 0 -T 0 temp.html ../resume.pdf
+# Use US Letter head.
+../wkhtmltopdf -s Letter -d 320 -B 0 -L 0 -R 0 -T 0 temp.html ../resume_${GIT}.pdf
 rm temp.html
 cd ..
-
+rm resume.pdf
+ln -s resume_${GIT}.pdf resume.pdf
